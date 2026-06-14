a0671d64a93d985eeaa389bb5c9e2cb05dfa2027
Ravinder Tiwari <ravindertiwari373@gmail.com>
2026-06-14 23:06:47 +0530

    Leetcode-344

---
+++ ReverseString.js
@@ -0,0 +1,22 @@
+
+class Solution {
+	public static String reverseString(String s) {
+		
+		char[] arr = s.toCharArray();
+		
+		int i = 0;
+		int j = arr.length - 1;
+		
+		while (i < j) {
+			char temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			
+			i++;
+			j--;
+		}
+		
+		return new String(arr);
+	}
+	
+}
