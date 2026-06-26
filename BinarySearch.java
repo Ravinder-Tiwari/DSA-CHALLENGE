142857afba6b65862991d7db9f36ca9280eef2d2
Ravinder Tiwari <ravindertiwari373@gmail.com>
2026-06-26 23:36:14 +0530

    log(n) solution implemented
    Refactor binary search algorithm from Java to JavaScript, implementing functions to find the first and last positions of a target in a sorted array.

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,24 +1,59 @@
-class Solution {
-    public int search(int[] nums, int target) {
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
 
-        int low = 0;
-        int high = nums.length - 1;
+    function findFirst() {
+        let left = 0;
+        let right = nums.length - 1;
+        let ans = -1;
 
-        while(low <= high){
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
 
-            int mid = low + (high - low) / 2;
-
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid] < target){
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
+            if (nums[mid] === target) {
+                ans = mid;
+                right = mid - 1; // Keep searching left
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
-        return -1;
+        return ans;
     }
-}
+
+    function findLast() {
+        let left = 0;
+        let right = nums.length - 1;
+        let ans = -1;
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+
+            if (nums[mid] === target) {
+                ans = mid;
+                left = mid + 1; // Keep searching right
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    const first = findFirst();
+
+    if (first === -1) {
+        return [-1, -1];
+    }
+
+    const last = findLast();
+
+    return [first, last];
+};
